b4e5701661481a6cf957f2c56abd140a3bc3b98e
kr-MATAGI <dev_matagi@kakao.com>
2022-08-23 22:21:11 +0900

    modify 들여쓰기

--- model/roberta_lstm_crf.py
+++ model/roberta_lstm_crf.py
@@ -43,40 +43,40 @@ class RoBERTa_LSTM_CRF(RobertaPreTrainedModel):
         self.post_init()
 
 
-def forward(
-        self, input_ids, attention_mask, token_type_ids, pos_tag_ids,
-        input_seq_len=None, labels=None, token_seq_len=None, eojeol_ids=None #, entity_ids=None
-):
-    # POS Embedding
-    # pos_tag_ids : [batch_size, seq_len, num_pos_tags]
-    pos_tag_1 = pos_tag_ids[:, :, 0]  # [batch_size, seq_len]
-    pos_tag_2 = pos_tag_ids[:, :, 1]  # [batch_size, seq_len]
-    pos_tag_3 = pos_tag_ids[:, :, 2]  # [batch_size, seq_len]
+    def forward(
+            self, input_ids, attention_mask, token_type_ids, pos_tag_ids,
+            input_seq_len=None, labels=None, token_seq_len=None, eojeol_ids=None #, entity_ids=None
+    ):
+        # POS Embedding
+        # pos_tag_ids : [batch_size, seq_len, num_pos_tags]
+        pos_tag_1 = pos_tag_ids[:, :, 0]  # [batch_size, seq_len]
+        pos_tag_2 = pos_tag_ids[:, :, 1]  # [batch_size, seq_len]
+        pos_tag_3 = pos_tag_ids[:, :, 2]  # [batch_size, seq_len]
 
-    pos_embed_1 = self.pos_embedding_1(pos_tag_1)  # [batch_size, seq_len, pos_tag_embed]
-    pos_embed_2 = self.pos_embedding_2(pos_tag_2)  # [batch_size, seq_len, pos_tag_embed]
-    pos_embed_3 = self.pos_embedding_3(pos_tag_3)  # [batch_size, seq_len, pos_tag_embed]
+        pos_embed_1 = self.pos_embedding_1(pos_tag_1)  # [batch_size, seq_len, pos_tag_embed]
+        pos_embed_2 = self.pos_embedding_2(pos_tag_2)  # [batch_size, seq_len, pos_tag_embed]
+        pos_embed_3 = self.pos_embedding_3(pos_tag_3)  # [batch_size, seq_len, pos_tag_embed]
 
-    outputs = self.roberta(input_ids=input_ids,
-                           attention_mask=attention_mask,
-                           token_type_ids=token_type_ids)
+        outputs = self.roberta(input_ids=input_ids,
+                               attention_mask=attention_mask,
+                               token_type_ids=token_type_ids)
 
-    sequence_output = outputs[0]  # [batch_size, seq_len, hidden_size]
-    concat_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
-    concat_embed = torch.concat([sequence_output, concat_embed], dim=-1)
-    lstm_out, _ = self.lstm(concat_embed)  # [batch_size, seq_len, hidden_size]
-    lstm_out = self.dropout(lstm_out)
-    logits = self.classifier(lstm_out)
+        sequence_output = outputs[0]  # [batch_size, seq_len, hidden_size]
+        concat_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
+        concat_embed = torch.concat([sequence_output, concat_embed], dim=-1)
+        lstm_out, _ = self.lstm(concat_embed)  # [batch_size, seq_len, hidden_size]
+        lstm_out = self.dropout(lstm_out)
+        logits = self.classifier(lstm_out)
 
-    # CRF
-    if labels is not None:
-        log_likelihood, sequence_of_tags = self.crf(emissions=logits, tags=labels, mask=attention_mask.bool(),
-                                                    reduction="mean"), self.crf.decode(
-            logits)  # mask=attention_mask.byte())
-        return log_likelihood, sequence_of_tags
-    else:
-        sequence_of_tags = self.crf.decode(logits)
-        return sequence_of_tags
+        # CRF
+        if labels is not None:
+            log_likelihood, sequence_of_tags = self.crf(emissions=logits, tags=labels, mask=attention_mask.bool(),
+                                                        reduction="mean"), self.crf.decode(
+                logits)  # mask=attention_mask.byte())
+            return log_likelihood, sequence_of_tags
+        else:
+            sequence_of_tags = self.crf.decode(logits)
+            return sequence_of_tags
 
 if "__main__" == __name__:
     from transformers import AutoConfig
