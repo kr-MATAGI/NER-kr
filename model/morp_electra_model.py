802642db3c1daf3e976b1db85ffcaab1697ff305
kr-MATAGI <dev_matagi@kakao.com>
2022-10-31 16:43:30 +0900

    조사 임베딩 주석 해제 2

--- model/morp_electra_model.py
+++ model/morp_electra_model.py
@@ -37,7 +37,7 @@ class ELECTRA_MECAB_MORP(ElectraPreTrainedModel):
 
         # POS tag embedding
         # self.ne_pos_embedding = nn.Embedding(self.num_ne_pos, self.pos_embed_out_dim // 2)
-        # self.josa_pos_embedding = nn.Embedding(self.num_josa_pos, self.pos_embed_out_dim)
+        self.josa_pos_embedding = nn.Embedding(self.num_josa_pos, self.pos_embed_out_dim)
 
         # Morp Embedding
         # self.morp_embedding = nn.Embedding(self.max_seq_len, self.max_seq_len)
