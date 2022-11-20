7a34f5d381d459d241f5d944e974a620fe737320
kr-MATAGI <dev_matagi@kakao.com>
2022-11-20 16:07:46 +0900

    호격/인용격 조사 제외 테스트

--- model/span_ner_model.py
+++ model/span_ner_model.py
@@ -24,13 +24,13 @@ class ElectraSpanNER(ElectraPreTrainedModel):
 
         self.span_combi_mode = "x,y"
         self.token_len_emb_dim = 50
-        self.max_span_width = 6 # 원본 깃에는 4
+        self.max_span_width = 8 # 원본 깃에는 4
         self.max_seq_len = 128
 
         self.span_len_emb_dim = 100
         ''' morp는 origin에서 {'isupper': 1, 'islower': 2, 'istitle': 3, 'isdigit': 4, 'other': 5}'''
         self.pos_emb_dim = 100
-        self.n_pos = 13 # NNP, NNG 통합
+        self.n_pos = 12 # 호격/인용격 조사 제외
         
         ''' 원본 Git에서는 Method 적용 개수에 따라 달라짐 '''
         self.input_dim = self.hidden_size * 2 + self.token_len_emb_dim + self.span_len_emb_dim + (self.pos_emb_dim * self.n_pos)
@@ -53,6 +53,7 @@ class ElectraSpanNER(ElectraPreTrainedModel):
         self.end_outputs = nn.Linear(self.hidden_size, 1)
         '''
 
+        # LSTM
         self.endpoint_span_extractor = EndpointSpanExtractor(input_dim=self.hidden_size,
                                                              combination=self.span_combi_mode,
                                                              num_width_embeddings=self.max_span_width,
@@ -220,7 +221,7 @@ class ElectraSpanNER(ElectraPreTrainedModel):
         mecab_tag2ids = {v: k for k, v in MECAB_POS_TAG.items()} # origin, 1: "NNG"
         target_tag_list = [ # NN은 NNG/NNP 통합
             "NNG", "NNP", "SN", "NNB", "NR",
-            "JKS", "JKC", "JKG", "JKO", "JKB", "JX", "JC" # JKV(호격 조사), JKQ(인용격 조사)
+            "JKS", "JKC", "JKG", "JKO", "JKB", "JX", "JC"#, "JKV", "JKQ"
         ]
         ''' 해당 되는 것의 pos_ids의 새로운 idx '''
         target_tag2ids = {mecab_tag2ids[x]: i for i, x in enumerate(target_tag_list)}
@@ -238,10 +239,11 @@ class ElectraSpanNER(ElectraPreTrainedModel):
                     for token_pos in curr_pos_ids[start_index:end_index+1]:
                         for key in target_keys:
                             if 1 == token_pos[key]:
-                                if 0 == key or 1 == key:
-                                    span_pos[0] = 1
-                                else:
-                                    span_pos[target_tag2ids[key]-1] = 1
+                                # if 0 == key or 1 == key:
+                                #     span_pos[0] = 1
+                                # else:
+                                #
+                                span_pos[target_tag2ids[key]] = 1
                     #     print(token_pos, start_index, end_index, curr_pos_ids[start_index:end_index+1].size())
                     # print("SPAN POS: ", span_pos)
                     # input()
