3cdafe86b8c17e848a3e64b1bb8ea83efbe821dc
kr-MATAGI <dev_matagi@kakao.com>
2023-01-03 16:48:17 +0900

    Concat Adding Wordpiece LSTM

--- model/char_electra_lstm_crf.py
+++ model/char_electra_lstm_crf.py
@@ -23,12 +23,12 @@ class CHAR_ELECTRA_POS_LSTM(ElectraPreTrainedModel):
         self.num_flag_pos = 15 # "##" 대신 마지막 인덱스에 1
 
         ''' POS Embedding '''
-        # self.pos_embedding_1 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
-        # self.pos_embedding_2 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
-        # self.pos_embedding_3 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
+        self.pos_embedding_1 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
+        self.pos_embedding_2 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
+        self.pos_embedding_3 = nn.Embedding(self.num_pos_labels, self.pos_embed_out_dim)
 
         ''' POS Bit Flag'''
-        self.pos_flag_embedding = nn.Embedding(self.num_flag_pos, self.pos_embed_dim)
+        # self.pos_flag_embedding = nn.Embedding(self.num_flag_pos, self.pos_embed_dim)
 
         '''
             @ Note
@@ -38,21 +38,20 @@ class CHAR_ELECTRA_POS_LSTM(ElectraPreTrainedModel):
                 Use from_pretrained() to load the model weights.
         '''
         self.electra = ElectraModel.from_pretrained("monologg/kocharelectra-base-discriminator", config=config)
-        # self.dropout = nn.Dropout(self.dropout_rate)
 
         # LSTM
-        self.lstm_dim_size = config.hidden_size #+ (self.pos_embed_dim * self.num_flag_pos)
+        self.lstm_dim_size = config.hidden_size + (self.pos_embed_dim * 3)
         self.lstm = nn.LSTM(input_size=self.lstm_dim_size, hidden_size=(self.lstm_dim_size // 2),
                             num_layers=1, batch_first=True, bidirectional=True, dropout=self.dropout_rate)
 
         # Classifier
-        # self.classifier = nn.Linear(self.lstm_dim_size, config.num_labels)
+        self.classifier = nn.Linear(self.lstm_dim_size, config.num_labels)
         self.crf = CRF(num_tags=config.num_labels, batch_first=True)
 
         ''' 뒷 부분에서 POS Embedding 추가하는 거 '''
-        self.post_pos_embed_dim = config.hidden_size + (self.pos_embed_dim * self.num_flag_pos)
-        self.post_pos_embedding = MultiNonLinearClassifier(self.post_pos_embed_dim,
-                                                           config.num_labels, self.dropout_rate)
+        # self.post_pos_embed_dim = config.hidden_size + (self.pos_embed_dim * self.num_flag_pos)
+        # self.post_pos_embedding = MultiNonLinearClassifier(self.post_pos_embed_dim,
+        #                                                    config.num_labels, self.dropout_rate)
 
         # Initialize weights and apply final processing
         self.post_init()
@@ -65,15 +64,16 @@ class CHAR_ELECTRA_POS_LSTM(ElectraPreTrainedModel):
     #===================================
         # pos embedding
         # pos_tag_ids : [batch_size, seq_len, num_pos_tags]
-        '''
-        pos_tag_1 = pos_tag_ids[:, :, 0] # [batch_size, seq_len]
-        pos_tag_2 = pos_tag_ids[:, :, 1] # [batch_size, seq_len]
-        pos_tag_3 = pos_tag_ids[:, :, 2] # [batch_size, seq_len]
+        pos_embed_1 = self.pos_embedding_1(pos_tag_ids[:, :, 0]) # [batch_size, seq_len, pos_tag_embed]
+        pos_embed_2 = self.pos_embedding_2(pos_tag_ids[:, :, 1])  # [batch_size, seq_len, pos_tag_embed]
+        pos_embed_3 = self.pos_embedding_3(pos_tag_ids[:, :, 2])  # [batch_size, seq_len, pos_tag_embed]
 
-        pos_embed_1 = self.pos_embedding_1(pos_tag_1) # [batch_size, seq_len, pos_tag_embed]
-        pos_embed_2 = self.pos_embedding_2(pos_tag_2)  # [batch_size, seq_len, pos_tag_embed]
-        pos_embed_3 = self.pos_embedding_3(pos_tag_3)  # [batch_size, seq_len, pos_tag_embed]
-        '''
+        pos_embed_1 = F.relu(pos_embed_1)
+        pos_embed_2 = F.relu(pos_embed_2)
+        pos_embed_3 = F.relu(pos_embed_3)
+
+        ''' Add POS Embedding '''
+        # add_pos_embed = torch.sum(torch.stack([pos_embed_1, pos_embed_2, pos_embed_3], 0))
 
         electra_outputs = self.electra(input_ids=input_ids,
                                        attention_mask=attention_mask,
@@ -81,23 +81,25 @@ class CHAR_ELECTRA_POS_LSTM(ElectraPreTrainedModel):
         electra_outputs = electra_outputs.last_hidden_state # [batch_size, seq_len, hidden_size]
 
         # POS Flag
+        '''
         pos_flag_out = self.pos_flag_embedding(pos_ids)  # [batch, seq_len, num_pos, pos_emb_dim]
         pos_flag_out = F.relu(pos_flag_out)
         pos_flag_size = pos_flag_out.size()
         pos_flag_out = pos_flag_out.reshape(pos_flag_size[0], pos_flag_size[1], -1)
+        '''
 
-        # concat_pos_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
+        concat_pos_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
         # concat_embed = torch.concat([electra_outputs, pos_flag_out], dim=-1)
 
         # LSTM
-        lstm_out, _ = self.lstm(electra_outputs) # [batch_size, seq_len, hidden_size]
+        lstm_out, _ = self.lstm(concat_pos_embed) # [batch_size, seq_len, hidden_size]
 
         # Classifier
-        # logits = self.classifier(lstm_out) # [128, 128, 31]
+        logits = self.classifier(lstm_out) # [128, 128, 31]
 
         ''' 뒷 부분에서 POS Embedding 추가하는 거 '''
-        concat_pos_flag = torch.cat([lstm_out, pos_flag_out], dim=-1)
-        logits = self.post_pos_embedding(concat_pos_flag)
+        # concat_pos_flag = torch.cat([lstm_out, pos_flag_out], dim=-1)
+        # logits = self.post_pos_embedding(concat_pos_flag)
 
         # Get Loss
         # loss = None
